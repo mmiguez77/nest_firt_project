e902415f61c2e24c3a2a365be03085e79702df00
mmiguez77 <martinmiguez77@gmail.com>
2023-09-02 02:46:29 -0300

    Agregado método POST

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post } from '@nestjs/common'
 import { CarsService } from './cars.service'
+import { CreateCarDto } from './dto/create-car.dto'
 
 @Controller('cars')
 export class CarsController {
@@ -14,14 +15,14 @@ export class CarsController {
   }
 
   @Get(':id')
-  getCarById( @Param('id', ParseIntPipe) id: number ) {
+  getCarById( @Param('id', ParseUUIDPipe ) id: string ) {
     return this.carsService.findOneById( id )
   }
 
   @Post()
-  createCar( @Body() body:any ) {
-
-    return body
+  createCar( @Body() createCarDto:CreateCarDto ) {
+    this.carsService.create(createCarDto)
+    return createCarDto
 
   }
 
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,20 +1,26 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuid } from 'uuid'
+
+import { Car } from './interfaces/car.interface';
+import { CreateCarDto } from './dto/create-car.dto';
+
+
 
 @Injectable()
 export class CarsService {
 
-    private cars = [
+    private cars: Car[] = [
         {
-            id: 1,
+            id: uuid(),
             brand: 'Toyota',
             model: 'Corolla'
         },         
         {
-            id: 2,
+            id: uuid(),
             brand: 'Honda',
             model: 'Civic'
         },        {
-            id: 3,
+            id: uuid(),
             brand: 'Jeep',
             model: 'Cherokee'
         }
@@ -24,10 +30,19 @@ export class CarsService {
         return this.cars;
     }
 
-    findOneById( id: number ) {
+    findOneById( id: string ) {
         const car = this.cars.find((car) => car.id === id)
         if( !car ) { throw new NotFoundException(`Car with id ${id} not exist`) }
         return car
+    }
+
+    create( createCarDto: CreateCarDto) {
+        const newCar: Car = {
+            id: uuid(),
+            ...createCarDto
+        }
+        this.cars.push(newCar)
+        return newCar
     }
 
     
